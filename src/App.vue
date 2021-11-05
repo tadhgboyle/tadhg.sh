68e079b38e0125c0ba07ea2c45334213b22392d8
Tadhg <tadhgsmboyle@gmail.com>
2021-11-05 09:19:02 -0700

    initial

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="h-screen mx-auto bg-pink-50 textured grid place-items-center">
+        <div class="bg-purple-200 mt-auto mb-auto px-10 pt-10 pb-5 border-4 w-1/3 border-purple-400">
+            <h1 class="font-serif text-purple-500 text-6xl">
+                tadhg boyle
+            </h1>
+            <h5 class="font-mono text-purple-400 text-2xl pt-3 pb-5">
+                web developer
+                <br>
+                software engineer
+            </h5>
+            <div class="font-serif text-purple-400 pb-5 space-y-2">
+                <p>
+                    <span class="text-xl pr-2">👋</span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                </p>
+                <p>
+                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                </p>
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+            </div>
+            <div class="text-center text-pink-400 space-x-8">
+                <a href="https://github.com/Aberdeener" target="_blank">🖥️ <span class="hover:underline">github</span></a>
+                <a href="https://www.linkedin.com/in/tadhg-boyle/" target="_blank">💭 <span class="hover:underline">linkedin</span></a>
+                <a href="https://twitter.com/Aberd33n3r" target="_blank">🐦 <span class="hover:underline">twitter</span></a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style>
+.textured {
+    background-image: url("data:image/svg+xml,%3Csvg width='40' height='40' viewBox='0 0 40 40' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='%23DDD6FE' fill-opacity='1' fill-rule='evenodd'%3E%3Cpath d='M0 40L40 0H20L0 20M40 40V20L20 40'/%3E%3C/g%3E%3C/svg%3E");
+}
+</style>
