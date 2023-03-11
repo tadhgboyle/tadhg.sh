dda35c8db1a32d7d964cb9cfaa26cea633a43342
Tadhg Boyle <tadhgsmboyle@gmail.com>
2023-03-11 06:48:54 -0800

    celebrate

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,6 @@
                     for the last 5 years i've been developing my skills as a programmer by working on many open source projects and pairing with clients to create awesome products.
                 </p>
                 <p>
-                    after a fantastic internship at <a href="https://shopify.com">Shopify</a>, i'm currently on the hunt for full-time position in a development role.
-                </p>
-                <p>
                     discord: <a href="https://discord.com/users/271510274475819008"><span class="font-bold">aberdeener#0001</span></a>
                     <br>
                     email: <a href="mailto:me@tadhg.sh"><span class="font-bold">me@tadhg.sh</span></a>
